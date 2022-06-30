8015b620e9451394fc09f61eaaef76c5b7cfc070
Sai kumar Murali krishnan <smur0027@student.monash.edu>
2022-07-01 01:19:03 +1000

    Initial work

---
+++ paramless.jl
@@ -0,0 +1,232 @@
+using Random
+
+DEF_ATOL = 1e-8
+
+MAX_ITER = 1e6
+
+"""
+_within_bounds(vector, lower_bound, upper_bound)
+
+Checks if all elements of an array are contained within [lower_bound, upper_bound]
+
+
+"""
+function _within_bounds(vector::Array, lower_bound, upper_bound)
+
+    if lower_bound !== nothing && minimum(vector) < lower_bound
+        return false
+    end
+    if upper_bound !== nothing && maximum(vector) > upper_bound
+        return false
+    end
+    return true
+end
+
+function _attempt_point_mutation(vector::Array, mutation_epsilon::Float64)::Array
+    mutant = deepcopy(vector)
+    position = rand(1:length(vector), 1)
+
+    mutant[position] += rand([-1, 1], 1) * mutation_epsilon
+
+    return mutant
+
+end
+
+function point_mutation(vector::Array; kwargs...)::Array
+    is_inside = false
+    attempt = 0
+    mutant = deepcopy(vector)
+
+    if haskey(kwargs, :mutation_epsilon)
+        mutation_epsilon = kwargs[:mutation_epsilon]
+    end
+    if haskey(kwargs, :lower_bound)
+        lower_bound = kwargs[:lower_bound]
+    else
+        lower_bound = nothing
+    end
+
+    if haskey(kwargs, :upper_bound)
+        upper_bound = kwargs[:upper_bound]
+    else
+        upper_bound = nothing
+    end
+
+    while !is_inside
+        mutant = _attempt_point_mutation(vector, mutation_epsilon) # Modify their DNA
+        is_inside = _within_bounds(mutant, lower_bound, upper_bound) # Check if everything is contained
+        attempt += 1
+
+        if attempt > MAX_ITER
+            throw(ErrorException("Attempted too many mutations without producing anythin within bounds"))
+        end
+
+    end
+    return mutant
+end
+
+#, mutation_epsilon::Float64
+function point_mutation_distribution(vector::Array; kwargs...)::Array
+    # moves up a random point, and down a random point at the same time
+    if haskey(kwargs, :mutation_epsilon)
+        mutation_epsilon = kwargs[:mutation_epsilon]
+    end
+
+    mutant = deepcopy(vector)
+    pos_up, pos_down = rand(1:length(vector), 2)
+    adjusted_epsilon = mutation_epsilon
+    # taking care that no negative values are allowed
+    if mutant[pos_down] - mutation_epsilon < 0
+        adjusted_epsilon = mutant[pos_down]
+    end
+    mutant[pos_up] = mutant[pos_up] + adjusted_epsilon
+    mutant[pos_down] = mutant[pos_down] - adjusted_epsilon
+    return mutant
+end
+
+function one_norm_distance(u,v)
+    return sum(abs.(u-v))
+end 
+function distance_fitness_function(resident, mutant; kwargs...)
+
+    if haskey(kwargs, :target_surface)
+        target_surface = kwargs[:target_surface]
+    end
+
+    fitness_res = 1.0/one_norm_distance(resident, target_surface)
+    fitness_mut = 1.0/one_norm_distance(mutant, target_surface)
+    return fitness_res, fitness_mut
+end
+
+
+# IMPLEMENT GAUSSIAN HGELPER 
+
+function _gaussian_mutation_helper(x, mutation_epsilon::Float64, loc::Float64, width::Float64)
+    return mutation_epsilon .* (ℯ.^(-(x .- loc).^2 ./ width))
+end
+
+function _attempt_gaussian_mutation(vector::Array, mutation_epsilon::Float64, domain, width::Float64)::Array
+    location_idx = rand(1:length(vector), 1)[1]
+    location_val = domain[location_idx]
+    mutant = deepcopy(vector)
+
+    perturbation = _gaussian_mutation_helper(domain, mutation_epsilon, location_val, width * rand(Float64, 1)[1])
+    mutant .+= rand([-1, 1], 1)[1] .* perturbation
+
+    return mutant
+end
+
+
+function gaussian_mutation(vector::Array; kwargs...)::Array
+    #=
+    Should generalise this, abstract away method of mutation
+    , mutation_epsilon, domain, width, lower_bound=nothing, upper_bound=nothing
+    =#
+    # Parse args 
+    mutant = deepcopy(vector)
+    if haskey(kwargs, :mutation_epsilon)
+        mutation_epsilon = kwargs[:mutation_epsilon]
+    end 
+    if haskey(kwargs, :domain)
+        domain = kwargs[:domain]
+    end 
+    if haskey(kwargs, :width)
+        width = kwargs[:width]
+    end 
+    if haskey(kwargs, :lower_bound)
+        lower_bound = kwargs[:lower_bound]
+    else 
+        lower_bound = nothing 
+    end 
+    if haskey(kwargs, :upper_bound)
+        upper_bound = kwargs[:upper_bound]
+    else 
+        upper_bound = nothing 
+    end
+
+
+    is_inside = false
+    attempt = 0
+
+    while !is_inside
+        mutant = _attempt_gaussian_mutation(vector, mutation_epsilon, domain, width) #, lower_bound, upper_bound)
+        is_inside = _within_bounds(mutant, lower_bound, upper_bound)
+        attempt += 1
+        if attempt > MAX_ITER
+            throw(ErrorException("Attempted too many mutations without producing anythin within bounds"))
+        end
+    end
+    return mutant
+end
+
+function gaussian_mutation_distribution(vector::Array, mutation_epsilon::Float64, domain, width)::Array
+    #= moves up a random point, and down a random point at the same time
+    - Produces a smoothing effect around each of the points like a bump
+    =#
+    mutant = deepcopy(vector)
+    pos_up, pos_down = rand(1:length(vector), 2)
+    min_val = minimum(vector)
+
+    if mutant[pos_down] - mutation_epsilon < 0
+        adjusted_epsilon = min_val
+    else
+        adjusted_epsilon = mutation_epsilon
+    end
+    width = rand(Float64, 1) * width
+    perturbation_up = _gaussian_mutation_helper(
+        domain, adjusted_epsilon, pos_up, width)
+    perturbation_down = _gaussian_mutation_helper(
+        domain, adjusted_epsilon, pos_down, width)
+
+    mutant .+= perturbation_up
+    mutant .-= perturbation_down
+
+    return mutant
+end
+
+
+
+
+function evolution_step(resident_surface, fitness_function, mutation_function, atol; kwargs...)
+    """
+    One generation iteration
+    """
+    invasion = false
+    mutant = mutation_function(resident_surface; kwargs...)
+    fitness_resident, fitness_mutant = fitness_function(
+        resident_surface, mutant; kwargs...)
+    if fitness_resident < fitness_mutant && abs(fitness_resident - fitness_mutant) > atol
+        resident_surface = deepcopy(mutant)
+        invasion = true
+    end
+
+    return resident_surface, invasion
+end
+
+# return_time_series=false
+function evolve(initial_surface, fitness_function, mutation_function, iterations, atol=DEF_ATOL, seed=0; kwargs...)
+    """
+    Evolve 
+    Returns last resident , plus time series data if required
+    """
+    Random.seed!(seed)
+
+    last_entry_time = 0
+    resident = deepcopy(initial_surface)
+    seq = 0
+
+    time_series = []
+
+    previous_resident = zeros(size(initial_surface))
+
+    for step = 1:iterations
+        previous_resident = deepcopy(resident)
+        resident, invasion = evolution_step(
+            resident, fitness_function, mutation_function, atol; kwargs...)
+
+        if (invasion) # Update when a mutant gene takes over
+            push!(time_series, [step - last_entry_time, previous_resident])
+        end
+    end
+    return resident, time_series
+end
